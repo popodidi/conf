9fb8e7a6beb5d71715bdb72db36cadf4dcefeefe
hao <changhao@haostudio.cc>
2020-03-28 15:50:25 +0800

    move examples to example_test.go

---
+++ example_test.go
@@ -0,0 +1,54 @@
+package conf_test
+
+import (
+	"log"
+
+	"github.com/popodidi/conf"
+	"github.com/popodidi/conf/source/env"
+	"github.com/popodidi/conf/source/mock"
+	"github.com/popodidi/conf/source/yaml"
+)
+
+func Example_basic() {
+	type config struct {
+		Str   string `conf:"default:hello"`
+		Int   int
+		Float float64
+	}
+	var err error
+	var cfg config
+	tmpl, _ := conf.Template(&cfg, yaml.NewExporter())
+	log.Println(tmpl)
+	err = conf.Load(&cfg, env.New(), yaml.New("example_basic.yaml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("cfg.Str", cfg.Str)
+	log.Println("cfg.Int", cfg.Int)
+	log.Println("cfg.Float", cfg.Float)
+}
+
+func Example_source() {
+	type config struct {
+		Str   string
+		Int   int
+		Float float64
+	}
+
+	var cfg config
+	err := conf.Load(&cfg,
+		env.New(),
+		yaml.New("config.yaml"),
+		mock.New(map[string]string{
+			"STR":   "??",
+			"INT":   "12",
+			"FLOAT": "3.456",
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("cfg.Str", cfg.Str)
+	log.Println("cfg.Int", cfg.Int)
+	log.Println("cfg.Float", cfg.Float)
+}
